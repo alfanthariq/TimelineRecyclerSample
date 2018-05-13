6f7a8c3c1063c1d6bf516f88bd49d6cce7795418
alfanthariq <naffripperz@gmail.com>
2018-05-13 18:00:47 +0700

    Add dummy data

--- app/src/main/java/com/alfanthariq/timelinerecyclersample/KegiatanSource.java
+++ app/src/main/java/com/alfanthariq/timelinerecyclersample/KegiatanSource.java
@@ -12,6 +12,7 @@ public class KegiatanSource {
 
     public KegiatanSource(){
         list = new ArrayList<Kegiatan>();
+        list.add(new Kegiatan("12/05/2018", "Sabtu","", ""));
         list.add(new Kegiatan("12/05/2018", "Sabtu","09:00", "Persiapan berangkat"));
         list.add(new Kegiatan("12/05/2018", "Sabtu","10:00", "Sampai tujuan"));
         list.add(new Kegiatan("12/05/2018", "Sabtu","11:00", "Perjalanan ke spot diving"));
@@ -21,12 +22,14 @@ public class KegiatanSource {
         list.add(new Kegiatan("12/05/2018", "Sabtu","20:00", "Sharing"));
         list.add(new Kegiatan("12/05/2018", "Sabtu","21:00", "Acara bebas"));
 
+        list.add(new Kegiatan("13/05/2018", "Minggu","", ""));
         list.add(new Kegiatan("13/05/2018", "Minggu","09:00", "Makan pagi"));
         list.add(new Kegiatan("13/05/2018", "Minggu","10:00", "Berangkat ke spot surfing"));
         list.add(new Kegiatan("13/05/2018", "Minggu","11:00", "Mulai surfing"));
         list.add(new Kegiatan("13/05/2018", "Minggu","16:00", "Istirahat"));
         list.add(new Kegiatan("13/05/2018", "Minggu","17:00", "Kembali ke penginapan"));
 
+        list.add(new Kegiatan("14/05/2018", "Senin","", ""));
         list.add(new Kegiatan("14/05/2018", "Senin","09:00", "Makan pagi"));
         list.add(new Kegiatan("14/05/2018", "Senin","10:00", "Berangkat ke spot surfing"));
         list.add(new Kegiatan("14/05/2018", "Senin","11:00", "Mulai surfing"));
